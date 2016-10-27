86262e78bb74b806d22ce0df0fa3c24522c0c1ec
Yuanyuan Peng <yuanyuan.peng@stonybrook.edu>
2016-10-26 20:06:06 -0700

    TODO: finish Markov_chain.py

--- markov_chain.py
+++ markov_chain.py
@@ -3,3 +3,20 @@
 #ID: 108 734 720
 import numpy as np
 import matplotlib.pyplot as plt
+eigenvalue = 1
+
+def normlize(P):
+	return P
+# funtion take a input P p is the matrix
+
+def transistion(P):
+	return P
+#constructs a random matrix
+A = np.random.rand(3,2)
+#and normalizes each row so that it is a transition matrix
+print(A)
+#starts from a random (normalizes) probabilitty distribution  p 
+#and takes 50 steps to obtain  p_50
+
+#computes the stationary distribution: the eigenvalue of P.T with eigenvalue 1
+#to obtain p_stationary (normalize eigenvector) 
